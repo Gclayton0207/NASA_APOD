55883dc654c38a4de4fc36b38ac81266afd075d2
Gclayton <giovanni.gcda@gmail.com>
2022-01-29 20:55:11 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,45 @@
+$('#button').click(function () {
+    let data = $('#data').val()
+  
+    const key = 'H7lwDc9BBy04RuCTFn8KV5z8FZXX21QTOxwtuEQF'
+  
+    if (data) {
+      url = `https://api.nasa.gov/planetary/apod?api_key=${key}&date=${data}`
+    } else {
+      url = `https://api.nasa.gov/planetary/apod?api_key=${key}`
+    }
+  
+    $.ajax({
+      type: 'GET',
+      url: `https://api.nasa.gov/planetary/apod?api_key=${key}&date=${data}`,
+      success: function (response) {
+        exibeConteudo(response)
+      },
+      error: function () {
+        alert(
+          'Selecione uma data valida! As imagens estão disponíveis desde 16/06/1995 até o dia de hoje!'
+        )
+      }
+    })
+  })
+  
+  function exibeConteudo(response) {
+    let image = $('#foto')
+    let video = $('#video')
+  
+    if (response.media_type == 'image') {
+      image.removeClass('visibility')
+      video.addClass('visibility')
+    } else if (response.media_type == 'video') {
+      video.removeClass('visibility')
+      image.addClass('visibility')
+    }
+  
+    $('#picTitle').text(response.title)
+    image.attr('src', `${response.url}`)
+    video.attr('src', `${response.url}`)
+    $('#text').text(response.explanation)
+  }
+  
+  $('#button').click()
+  
