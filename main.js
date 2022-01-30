243d516027f214c4f1162cda40489b8b2aaf6c24
Gclayton <giovanni.gcda@gmail.com>
2022-01-30 12:51:24 -0300

    correcao js

--- main.js
+++ main.js
@@ -1,43 +1,42 @@
 $('#button').click(function () {
-    var data = $('#data').val()
-
+    let data = $('#data').val()
+  
     const key = 'H7lwDc9BBy04RuCTFn8KV5z8FZXX21QTOxwtuEQF'
-
+  
     if (data) {
-        url = `https://api.nasa.gov/planetary/apod?api_key=${key}&date=${data}`
+      url = `https://api.nasa.gov/planetary/apod?api_key=${key}&date=${data}`
     } else {
-        url = `https://api.nasa.gov/planetary/apod?api_key=${key}`
+      url = `https://api.nasa.gov/planetary/apod?api_key=${key}`
     }
-
+  
     $.ajax({
-        type: 'GET',
-        url: `https://api.nasa.gov/planetary/apod?api_key=${key}&date=${data}`,
-        success: function (response) {
-            exibirConteudo(response)
-        },
-        error: function () {
-            alert(
-                'Erro na API, por favor tente novamente. Recarregue a pagina ou mude a data, datas disponiveis: 16/06/1995 até o dia de hoje!'
-            )
-        }
+      type: 'GET',
+      url: `https://api.nasa.gov/planetary/apod?api_key=${key}&date=${data}`,
+      success: function (response) {
+        exibeConteudo(response)
+      },
+      error: function () {
+        alert(
+            'Erro na API, por favor tente novamente. Recarregue a pagina ou mude a data, datas disponiveis: 16/06/1995 até o dia de hoje!'
+        )
+      }
     })
-})
-
-function exibirConteudo(response) {
-    var image = $('#foto')
-    var video = $('#video')
-
+  })
+  
+  function exibeConteudo(response) {
+    let image = $('#foto')
+    let video = $('#video')
+  
     if (response.media_type == 'image') {
-        image.removeClass('visibility')
-        video.addClass('visibility')
+      image.removeClass('visibility')
+      video.addClass('visibility')
     } else if (response.media_type == 'video') {
-        video.removeClass('visibility')
-        image.addClass('visibility')
+      video.removeClass('visibility')
+      image.addClass('visibility')
     }
-
+  
     $('#titulo').text(response.title)
     image.attr('src', `${response.url}`)
     video.attr('src', `${response.url}`)
     $('#text').text(response.explanation)
-}
-
+  }
